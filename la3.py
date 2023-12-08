b450e6fedb82d82675805e6b663b8d137d96906a
Meriem <114213504+gaia95@users.noreply.github.com>
2023-12-08 17:48:40 +0100

    User input

---
+++ la3.py
@@ -0,0 +1,12 @@
+# Input funktionen 
+print("Skriv ditt förnamn:")
+fn = input() # funktion som hämtar info från user
+
+print("Skriv ditt efternamn:")
+en = input()
+
+print("Hur gammal är du?")
+age = input()
+
+print("Hej " + fn + " " + en)
+print("Din ålder: " + age)
