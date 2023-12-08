a969e5d6b2d775232bb7542f2eea986643e1b185
Meriem <114213504+gaia95@users.noreply.github.com>
2023-12-08 17:59:37 +0100

    simple loop

---
+++ 2ld2.py
@@ -0,0 +1,16 @@
+# for loopar.
+
+for i in range(5): # 0 - 4
+    print(i)
+print("------")
+
+for varnamn in range(4,10): # startar på 4 fram till 9
+    print(varnamn)
+print("------")
+
+for x in range(1,20,2): # start, slut, antal hopp 
+    print(x)
+print("------")
+
+for varnamn2 in range(10, -10,-1): # start 10, slut -10, antal hopp
+    print(varnamn2)
