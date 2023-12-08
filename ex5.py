6993cdac937973813fa3a8d3e88211a8c06e83ac
Meriem <114213504+gaia95@users.noreply.github.com>
2023-12-08 17:09:52 +0100

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = "Zed A. Shawn" # Variabel med sparat värde 
+my_age = 35
+my_height = 74 # inches 
+my_weight = 180 # lbs
+my_eyes = "Blue"
+my_teeth = "White"
+my_hair = "Brown"
+
+# f strängar är ett annat sätt att skriva ut text strängar och variablar i samma kodrad.
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height}.")
+print(f"He's {my_weight} pounds heavy.")
+print(f"Actually not that heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If i add {my_age}, {my_height}, and {my_weight} i get {total}.")
