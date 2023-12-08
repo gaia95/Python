b3ea3c308b2aab25670c2a99595748d910037346
Meriem <114213504+gaia95@users.noreply.github.com>
2023-12-08 17:26:59 +0100

    Variables and strings

---
+++ ex9.py
@@ -0,0 +1,15 @@
+
+days = "Mon Tues Wen Thur Fri Sat Sun" # Variabel med värden 
+months = "\njan\nfeb\nmars\napr\nmay\njun\njul\naug\nsep\nnov\ndec\n" # \n rad byte för en text sträng
+
+print("Here are the day's of the week:", days) # text sträng och var 'days' hämtar värdet i variabeln och skriver ut det
+print("Here are the months:", months)
+
+# Everything after the 3 qutoation marks are going to be text str.
+
+print(""" 
+Theres something going on here.
+With the three double quotes,
+we'll be able to type as much as we want,
+doesn't matter how many lines
+""") 
